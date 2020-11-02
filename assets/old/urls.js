2938d11f49845844c0fc096be632254eb4f4515a
kevin lee <kevin.c.lee26@gmail.com>
2020-11-02 13:21:43 -0800

    updated

--- assets/old/urls.js
+++ assets/old/urls.js
@@ -9,7 +9,7 @@ var othersRemCountList=[];
 var timeFrame=20;
 var tableRecords=20;
 compInfo.map(comp=>comp['name']).forEach(name=>record[name]={'features':[], 'censusCount':{}});
-var timeInterval=60000;
+var timeInterval=90000;
 var prevFeatures=[];
 var updatedFeatures=[];
 var censusFeatures=[];
